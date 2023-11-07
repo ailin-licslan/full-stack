165f035717a8894db3075b398ec314dd959b7be4
licslan <licslan@sina.com>
2023-11-07 16:11:12 +0800

    注释说明

--- react-basic-play/src/index.js
+++ react-basic-play/src/index.js
@@ -2,6 +2,11 @@ import React from 'react';  //框架核心包
 import ReactDOM from 'react-dom/client'; //专门做渲染相关的包
 
 import './index.css'; //应用的全局样式文件
+
+/**
+ * 特别说明 下面的每个注释单独放开 就可以启动每个学习示例了  ^_^
+ * */
+
 //import App from '../src/apps/App'; //引入根组件
 
 //import AppFun from '../src/apps/AppFun'; // 引入函数组件
@@ -10,19 +15,21 @@ import './index.css'; //应用的全局样式文件
 
 //import AppHooks from '../src/apps/AppHooks'; // 引入函数组件Hooks
 
-import AppMobx from "./apps/AppMobx"; // 引入状态管理组件
-import AppMobxComputed from "./apps/AppMobxComputed";
+//import AppMobx from "./apps/AppMobx"; // 引入状态管理组件
+
+//import AppMobxComputed from "./apps/AppMobxComputed";
+
 import AppMobxModule from "./apps/AppMobxModule";
 
 //渲染根组件APP到id=root的DOM节点上
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+
     /*<AppFun/>*/
     /*<AppHooks/>*/
     /*<AppRouter/>*/
     /*<AppMobx/>*/
     /*<AppMobxComputed/>*/
-
     <AppMobxModule/>
 );
 
